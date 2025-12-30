f133d8388a0c494f84d5d31263466dd4cc3184a0
Mifthahul Amri <mifthahulamri@gmail.com>
2025-12-31 06:31:49 +0700

    nambah slice program dan pendaftaran

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('berita/<int:id>/', views.detail_berita, name='detail_berita'),
+    path('program/sarjana/', views.program_sarjana, name='program_sarjana'),
+    path('program/profesi/', views.program_profesi, name='program_profesi'),
+    path('program/magister/', views.program_magister, name='program_magister'),
+    path('pendaftaran/dosen/', views.pendaftaran_dosen, name='pendaftaran_dosen'),
+    path('pendaftaran/mahasiswa/', views.pendaftaran_mahasiswa, name='pendaftaran_mahasiswa'),
 ]
 
 if settings.DEBUG:
--- website/views.py
+++ website/views.py
@@ -14,3 +14,18 @@ def index(request):
 def detail_berita(request, id):
     berita = get_object_or_404(Berita, id=id)
     return render(request, 'detail_berita.html', {'berita': berita})
+
+def program_sarjana(request):
+    return render(request, 'program_sarjana.html')
+
+def program_profesi(request):
+    return render(request, 'program_profesi.html')
+
+def program_magister(request):
+    return render(request, 'program_magister.html')
+
+def pendaftaran_dosen(request):
+    return render(request, 'pendaftaran_dosen.html')
+
+def pendaftaran_mahasiswa(request):
+    return render(request, 'pendaftaran_mahasiswa.html')
